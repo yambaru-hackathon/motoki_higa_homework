fbfba7d9163bf2e058387a717e812cd4a5b9eaef
kurashin234 <sabu18802@gmail.com>
2024-02-14 23:57:23 +0900

    feature/riverpod_1

---
+++ lib/counter.dart
@@ -0,0 +1,26 @@
+import 'package:riverpod_annotation/riverpod_annotation.dart';
+
+part 'counter.g.dart';
+
+@riverpod
+class Counter extends _$Counter{
+
+  @override
+  int build() => 0;
+
+  void incrementCounter() {
+    state++;
+  }
+
+  void decrementCounter() {
+    state--;
+  }
+
+  void resetCounter(){
+    state = 0;
+  }
+
+  void multiplyCounter(){
+    state *= state;
+  }
+}
